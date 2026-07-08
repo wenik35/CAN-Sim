9684373f28ad842b2a0195ebf3a42afe4e6213e6
NiklasW <niwer0305@gmx.de>
2026-07-08 17:34:23 +0200

    servo working

--- can-f413-makefile/Src/main.c
+++ can-f413-makefile/Src/main.c
@@ -230,7 +230,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 96;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 4294967295;
+  htim2.Init.Period = 19999;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
