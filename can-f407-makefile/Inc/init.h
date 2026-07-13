07144f3d9c6c90f025ec885e18946e2821b0b16f
LtSchneefall <149060067+LtSchneefall@users.noreply.github.com>
2026-07-13 15:10:39 +0200

    Update to 407

--- can-f407-makefile/Inc/init.h
+++ can-f407-makefile/Inc/init.h
@@ -1,56 +1,19 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H
-#define __MAIN_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
+#include "usb_host.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-#include "init.h"
 
-/* USER CODE END Includes */
+#ifndef __INIT_H
+#define __INIT_H
 
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
 
-/* USER CODE END ET */
+void init(void);
 
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
 
-/* USER CODE END EC */
+// from main.c
+void SystemClock_Config(void);
 
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
+// needed by SystemClock_Config() and MX_GPIO_Init()
 
-/* USER CODE END EM */
-
-/* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
